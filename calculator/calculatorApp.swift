ecedcd905f9fd425ed1774700f978f70572b096c
wangtianwei <1471355406@qq.com>
2021-05-23 14:37:19 +0800

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by 王天伟 on 2021/5/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
